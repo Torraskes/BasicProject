5578eb33e97bf89a02a75cca4a98dd402f1ff48e
Torraskes <78329836+Torraskes@users.noreply.github.com>
2021-02-03 08:54:06 -0600

    Commit Services and Repository Interfaces, and ProductViewModel

---
+++ Solution1/MyStore.Application/ViewModels/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using MyStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Application.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ Solution1/MyStore.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyStore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+    }
+}
