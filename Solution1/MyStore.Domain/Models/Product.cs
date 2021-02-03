e9ff6e179d1e95a1b2fc63e05578104e7fe7be28
Torraskes <78329836+Torraskes@users.noreply.github.com>
2021-02-02 21:12:33 -0600

    Add PRoduct, Sote and Transaction Model

---
+++ Solution1/MyStore.Domain/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }        
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public float Cost { get; set; }
+        public int InExistance { get; set; }
+        public string Brand { get; set; }
+    }
+}
---
+++ Solution1/MyStore.Domain/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStore.Domain.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int prodId { get; set; }
+        public int storeId { get; set; }
+        public string Date { get; set; }
+    }
+}
