4b5c7f57b203f14b9f75c59a8091cadc573d6c92
Torraskes <78329836+Torraskes@users.noreply.github.com>
2021-02-03 10:25:57 -0600

    Commit changes for Presentation Product Controller, Views, Styling, Authentication

---
+++ Solution1/MyStore.MVC/Controllers/Productcontroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyStore.Application.Interfaces;
+using MyStore.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyStore.MVC.Controllers
+{
+    [Authorize]
+    public class Productcontroller : Controller
+    {
+        private IProductService _productService;
+
+        public Productcontroller(IProductService productServic)
+        {
+            _productService = productServic;
+        }
+
+
+        public IActionResult Index()
+        {
+            ProductViewModel model = this._productService.GetProducts();
+            return View(model);
+        }
+    }
+}
