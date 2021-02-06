6f485df9b0efc9bb88ffe7f761d83c65fa2dd126
Torraskes <78329836+Torraskes@users.noreply.github.com>
2021-02-06 13:51:14 -0600

    Updateing Migrations and eliminating blank spaces

--- Solution1/MyStore.Infra.Data/Migrations/20210206195005_TableUpdates.Designer.cs
+++ Solution1/MyStore.Infra.Data/Migrations/20210206195005_TableUpdates.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyStore.Infra.Data.Context;
 
 namespace MyStore.Infra.Data.Migrations
 {
     [DbContext(typeof(MyStoreDBContext))]
-    partial class MyStoreDBContextModelSnapshot : ModelSnapshot
+    [Migration("20210206195005_TableUpdates")]
+    partial class TableUpdates
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
